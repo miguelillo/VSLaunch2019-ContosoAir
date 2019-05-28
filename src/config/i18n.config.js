a58bef4ec8afa74a8a64d364076382068bc2a459
Miguel Ángel Navarro Vera <manavarro@encamina.com>
2019-05-28 22:30:02 +0200

    Cambiados los locales.

--- src/config/i18n.config.js
+++ src/config/i18n.config.js
@@ -3,10 +3,10 @@ const i18n = require('i18n');
 const configureI18N = (root_path) => {
     i18n.configure({
         objectNotation: true,
-        locales: ['es'],
+        locales: ['en', 'es'],
         directory: root_path + '/locales'
     });
-    
+
     return i18n;
 }
 
